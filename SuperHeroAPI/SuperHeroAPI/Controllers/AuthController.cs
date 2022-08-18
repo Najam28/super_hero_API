3304fc3f3bce414b29433dc7758b8a78e114e9fd
Muhammad Najam Ul Hassan <93036245+Najam28@users.noreply.github.com>
2022-08-18 09:38:51 +0500

    register and login user, create and verify password
    register and login user, create and verify password

---
+++ SuperHeroAPI/SuperHeroAPI/Controllers/AuthController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new User();
+        [HttpPost ("register")]
+        public async Task<ActionResult<User>> Register (UserDto request)
+        {
+            CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
+            user.UserName = request.UserName;
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+            return Ok(user);
+        }
+
+        [HttpPut("login")]
+        public async Task<ActionResult<string>> Login(UserDto request)
+        {
+            if (user.UserName != request.UserName)
+            {
+                return BadRequest("User not Found");
+            }
+            if (!VerifyPassword(request.Password, user.PasswordHash, user.PasswordSalt))
+            {
+                return BadRequest("Wrong Password");
+            }
+            return Ok("Great");
+            //if (user.UserName == request.UserName)
+            //    return Ok("Great Work");
+            //if (!VerifyPassword(request.Password, user.PasswordSalt, user.PasswordHash))
+            //    return BadRequest("Wrong Password");
+            //return BadRequest("User not found");
+        }
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+        private bool VerifyPassword(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
+            }
+        }
+    }
+}
