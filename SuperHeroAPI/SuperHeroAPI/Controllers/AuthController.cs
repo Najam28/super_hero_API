ed3d5c2e89183996f24872fc20f8d77f21c48d54
Muhammad Najam Ul Hassan <93036245+Najam28@users.noreply.github.com>
2022-08-18 11:53:10 +0500

    generate tokens
    generate tokens

--- SuperHeroAPI/SuperHeroAPI/Controllers/AuthController.cs
+++ SuperHeroAPI/SuperHeroAPI/Controllers/AuthController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Security.Cryptography;
 
 namespace SuperHeroAPI.Controllers
@@ -8,7 +11,14 @@ namespace SuperHeroAPI.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        
         public static User user = new User();
+        private readonly IConfiguration configuration;
+        public AuthController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
         [HttpPost ("register")]
         public async Task<ActionResult<User>> Register (UserDto request)
         {
@@ -30,12 +40,29 @@ namespace SuperHeroAPI.Controllers
             {
                 return BadRequest("Wrong Password");
             }
-            return Ok("Great");
+            var authtoken = CreateToken(user);
+            return Ok(authtoken);
             //if (user.UserName == request.UserName)
             //    return Ok("Great Work");
             //if (!VerifyPassword(request.Password, user.PasswordSalt, user.PasswordHash))
             //    return BadRequest("Wrong Password");
             //return BadRequest("User not found");
+        }
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>();
+            {
+                new Claim(ClaimTypes.Name, user.UserName);
+            };
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(configuration.GetSection("AppSettings:Token").Value));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds
+                );
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+            return jwt;
         }
         private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
         {
